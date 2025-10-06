1e1bd6c8afe1df0787024659c2ffc89e9c7cbbd6
coolguy <111678982+denisexploitsandother@users.noreply.github.com>
2025-10-06 22:44:49 +0200

    arasdasfegdfh

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If the host is 'localhost:NUMBER', the NUMBER must be the port setting.
-    host: "https://polygoned.onrender.com/",
+    host: "https://polygoned.onrender.com/:3000",
 
     // Which port to run the web server on.
     port: 3000,
@@ -30,7 +30,7 @@ module.exports = {
             LOAD_ON_MAINSERVER: false,
             // This is very important where the host is actually being hosted. Keep in mind that the ports gets changed to 3000, to 3001, and it repeats with the other servers.
             // Keep in mind that if "LOAD_ON_MAINSERVER" is set to true then this code will be replaced with the main server's host instead.
-            HOST: "https://polygoned.onrender.com/",
+            HOST: "https://polygoned.onrender.com/:3001",
             // The port where to host from.
             // Keep in mind that if "LOAD_ON_MAINSERVER" is set to true then this code will be replaced with the main server's port instead.
             PORT: 3001,
