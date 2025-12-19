3d2ebf24be7dc5d9be0a9f975d4f291d57cb2574
unknown <denisspace1@gmail.com>
2025-12-19 18:18:03 +0100

    config to render

--- server/config.js
+++ server/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     // Main Menu
     main_menu: "index.html", // Where the main menu is located (in the /public folder).
-    host: "localhost:3000", // Game server domain. If the host is 'localhost:NUMBER', the NUMBER must be the port setting.
-    port: 3000, // Which port to run the web server on.
+    host: "polygoned.onrender.com", // Game server domain. If the host is 'localhost:NUMBER', the NUMBER must be the port setting.
+    port: 8080, // Which port to run the web server on.
 
     // Server
     visible_list_interval: 250, // How often to update the list of the entities that players can see. Has effects of when entities are activated.
@@ -11,7 +11,7 @@ module.exports = {
 
     servers: [ // Make sure to change the HOST, PORT and SERVER_ID between servers!
         {
-            share_client_server: false, // Only one server at a time can have this enabled.
+            share_client_server: true, // Only one server at a time can have this enabled.
             // The above is required if your VM (the machine that hosts the website stuff) doesn't support multi-ports and forces everything through the main server.
             // This also overrides the below HOST and PORT settings to be identical to the main server's HOST/PORT (by default, 3000).
 
