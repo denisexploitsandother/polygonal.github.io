f5d4bb1a7ea1863706e931753a0fd1fd815a38e7
unknown <denisspace1@gmail.com>
2025-12-19 22:40:51 +0100

    config to render 4

--- server/config.js
+++ server/config.js
@@ -30,12 +30,12 @@ module.exports = {
             }
         },
         {
-            share_client_server: false, // Only one server at a time can have this enabled.
+            share_client_server: true, // Only one server at a time can have this enabled.
             // The above is required if your VM (the machine that hosts the website stuff) doesn't support multi-ports and forces everything through the main server.
             // This also overrides the below HOST and PORT settings to be identical to the main server's HOST/PORT (by default, 3000).
 
-            host: "localhost:3002", // Server host location.
-            port: 3002, // The port on the server.
+            host: "polygoned.onrender.com", // Server host location.
+            port: 8080, // The port on the server.
             id: "lod", // (<HOST>/#<SERVER_ID>)
             featured: false,
 
